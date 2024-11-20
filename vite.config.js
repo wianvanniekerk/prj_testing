401bb1d0a521c1ba238d65f8218a7ab1ce2b4952
Wian <140880006+wianvanniekerk@users.noreply.github.com>
2024-11-20 20:22:13 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Load environment variables
 const backendUrl = 'https://prjtesting-production.up.railway.app';
 
 export default defineConfig({
